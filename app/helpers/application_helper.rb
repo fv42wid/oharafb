e3f5dbe788934e19d1d7005037d2636c2991a237
Frank Vinci <fv42wid@yahoo.com>
2017-09-24 12:05:34 -0400

    active link working, roster page next

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def active_link?(action)
+    if(action == 'home' && current_page?('/'))
+      return 'is-active'
+    elsif(current_page?(controller: 'pages', action: action))
+      return 'is-active'
+    end
+  end
+
 end
